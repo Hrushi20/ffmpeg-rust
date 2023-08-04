cc504536a9d15224aac10b36c5c081e3f449a525
Pc <pc@Pcs-MacBook-Air.local>
2023-08-04 18:40:27 +0530

    Remove generated.rs file

--- src/generated.rs
+++
@@ -1,5 +0,0 @@
-
-#[link(wasm_import_module = "wasmedge_ffmpeg")]
-extern "C" {    
-                #[link_name = "wasmedge_ffmpeg_avFormatOpenInput"] 
-                pub fn avFormatOpenInput(file_ptr: *const u8, file_len: usize) -> u32;}
